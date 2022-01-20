40cc48ceffb840f0615950b593ad42dd8994c558
Shira Semo <shirasemo3@gmail.com>
2022-01-20 15:20:13 +0200

    added DataBase class

---
+++ src/main/java/DataBase.java
@@ -0,0 +1,19 @@
+import Models.Table;
+
+import java.util.HashMap;
+
+public class DataBase {
+    private String name;
+    private HashMap<String, Table> tables;
+
+    public DataBase(String name) {
+        this.name = name;
+        this.tables = new HashMap<>();
+    }
+    public void create(String name) {
+        this.tables.put(name, new Table());
+    }
+    public void remove(String name) {
+        this.tables.remove(name);
+    }
+}
